759803eca4fdffed4806b033ea47e3c768bc3b18
Jose Balado <JoseBalado@users.noreply.github.com>
2019-02-19 18:01:41 +0000

    Add js.map and watch for file changes to script npm run watch

--- index.ts
+++ index.ts
@@ -12,3 +12,52 @@ const fc2 = fb.ap(fa.ap(some(f)))
 const fc3 = fb.ap(fa.map(f))
 const fc4 = liftA2(option)(f)(fa)(fb)
 const fc5 = sequenceT(option)(fa, fb).map(([a, b]) => f(a)(b))
+
+interface Iterator<T> {
+  next(value?: any): IteratorResult<T>;
+  return?(value?: any): IteratorResult<T>;
+  throw?(e?: any): IteratorResult<T>;
+}
+
+interface IteratorResult<T> {
+  done: boolean;
+  value: T;
+}
+
+class Component {
+  constructor (public name: string) {}
+}
+
+class Frame implements Iterator<Component> {
+
+  private pointer = 0;
+
+  constructor(public name: string, public components: Component[]) {}
+
+  public next(): IteratorResult<Component> {
+    if (this.pointer < this.components.length) {
+      return {
+        done: false,
+        value: this.components[this.pointer++]
+      }
+    } else {
+      return {
+        done: true,
+        value: { name: 'finish'}
+      }
+    }
+  }
+
+}
+
+let frame = new Frame("Door", [new Component("top"), new Component("bottom"), new Component("left"), new Component("right")]);
+let iteratorResult1 = frame.next(); //{ done: false, value: Component { name: 'top' } }
+let iteratorResult2 = frame.next(); //{ done: false, value: Component { name: 'bottom' } }
+let iteratorResult3 = frame.next(); //{ done: false, value: Component { name: 'left' } }
+let iteratorResult4 = frame.next(); //{ done: false, value: Component { name: 'right' } }
+let iteratorResult5 = frame.next(); //{ done: true }
+
+//It is possible to access the value of iterator result via the value property:
+let component = iteratorResult1.value; //Component { name: 'top' }
+
+console.log('asdac');
