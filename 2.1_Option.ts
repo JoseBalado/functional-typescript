c53a928419e6f9b370bf621f22a4c591f1b25a3d
Jose Balado <JoseBalado@users.noreply.github.com>
2019-04-24 11:55:20 +0100

    2.1 Option

---
+++ 2.1_Option.ts
@@ -0,0 +1,67 @@
+// import { some, option } from 'fp-ts/lib/Option'
+// import { liftA2, sequenceT } from 'fp-ts/lib/Apply'
+
+const xs = [1, 2, 3]
+const double = (x: number): number => x * 2
+
+const ys = []
+for (var i = 0; i < xs.length; i++) {
+  ys.push(double(xs[i]))
+}
+
+const zs = xs.map(double)
+
+console.log(zs)
+
+const f: { [key: number]: number } = {
+  1: 2,
+  2: 4,
+  3: 6
+  }
+
+
+console.log(f)
+
+type Option<A> = None<A> | Some<A>
+class None<A> {
+  readonly _tag = 'None'
+  map<B>(f: (a: A) => B): Option<B> {
+    return none
+  }
+}
+class Some<A> {
+  readonly _tag = 'Some'
+  constructor(readonly value: A) { }
+  map<B>(f: (a: A) => B): Option<B> {
+    return some(f(this.value))
+  }
+}
+
+const none: Option<never> = new None()
+const some = <A>(a: A): Option<A> => new Some(a)
+
+const inverse = (x: number): Option<number> =>
+  x === 0 ? none : some(1 / x)
+
+const doubleInverse = (x: number): Option<number> => inverse(x).map(double)
+console.log(
+doubleInverse(2)
+) // Some(1)
+
+console.log(
+doubleInverse(0)
+) // None
+
+const inc = (x: number): number => x + 1
+
+console.log(
+inverse(0)
+  .map(double)
+  .map(inc) // None
+)
+
+console.log(
+inverse(4)
+  .map(double)
+  .map(inc) // Some(1.5)
+)
