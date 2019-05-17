265ffc190b826a83e6b035648b7ace19e2b9c1df
Jose Balado <JoseBalado@users.noreply.github.com>
2019-05-17 17:41:54 +0100

    Contravariant Functor

--- 5_Contravariant_Functor.ts
+++ 5_Contravariant_Functor.ts
@@ -1,127 +1,29 @@
-type Either<L, A> = Left<L, A> | Right<L, A>
+// Contravariant Functor
+type Predicate<A> = (a: A) => boolean
 
-class Left<L, A> {
-  readonly _tag = "Left"
-  constructor(readonly value: L) {}
-  map<B>(f: (a: A) => B): Either<L, B> {
-    return left(this.value)
+const contramap = <A, B>(
+  f: (b: B) => A,
+  predicate: Predicate<A>
+  ): Predicate<B> => {
+    return b => predicate(f(b))
   }
-}
 
-class Right<L, A> {
-  readonly _tag = "Right"
-  constructor(readonly value: A) {}
-  map<B>(f: (a: A) => B): Either<L, B> {
-    return right(f(this.value))
-  }
-}
+const isAdult: Predicate<number> = age => age >= 18
 
-const left = <L, A>(l: L): Either<L, A> => new Left(l)
-const right = <L, A>(a: A): Either<L, A> => new Right(a)
-
-const double = (x: number): number => x * 2
-
-const inverse = (x: number): Either<string, number> =>
-  x === 0 ? left("cannot divide by zero") : right(1 / x)
-
-const doubleInverse = (x: number): Either<string, number> =>
-  inverse(x).map(double)
-
-console.log(
-doubleInverse(2) // Right(1)
-)
-
-console.log(
-doubleInverse(0) // Left('cannot divide by zero')
-)
-
-const inc = (x: number): number => x + 1
-
-console.log(
-inverse(0)
-  .map(double)
-  .map(inc) // Left('cannot divide by zero')
-)
-
-console.log(
-inverse(4)
-  .map(double)
-  .map(inc) // Right(1.5)
-)
-
-// Identity Functor
-class Identity<A> {
-  constructor(readonly value: A) { }
-  map<B>(f: (a: A) => B): Identity<B> {
-    return new Identity(f(this.value))
-  }
-}
-
-interface User {
+interface Person {
   name: string
+  age: number
 }
 
-const identity = new Identity(5)
-const identity2 = new Identity({ name: 'John', surname: 'Doe' })
-
-const nameLength = (text : User): number => text.name.length
-
-console.log(identity)
-console.log(identity2)
-
-console.log(
-identity
-  .map(double)
+const isPersonAdult: Predicate<Person> = contramap(
+  p => p.age,
+  isAdult
 )
 
 console.log(
-identity2
-  .map(nameLength)
+isPersonAdult({ name: 'Giulio', age: 45 })
 )
 
-// Functor Array
-const functorArray = {
-  map: <A, B>(f: (a: A) => B, fa: Array<A>): Array<B> =>
-    fa.map(f)
-}
-
-const arrayNumbers: Array<number> = [1,2,3]
-
-const addOne = (x : number): number => x + 1
-
-console.log('functorArray',
-functorArray.map(addOne, arrayNumbers)
+console.log(
+isPersonAdult({ name: 'Matilde', age: 2 })
 )
-
-// Functor IO
-class IO<A> {
-  constructor(readonly run: () => A) { }
-  map<B>(f: (a: A) => B): IO<B> {
-    return new IO(() => f(this.run()))
-  }
-}
-
-const log = (s: unknown): IO<void> => new IO(() => console.log(s))
-const helloIO = log('Hello IO')
-helloIO.run()
-
-const io = new IO(() => 'Hello IO')
- io
-  .map(text => text + ' again!!!')
-  .map(console.log.bind(console))
-  .run()
-
-
-// Functor Task
-class Task<A> {
-  constructor(readonly run: () => Promise<A>) { }
-  map<B>(f: (a: A) => B): Task<B> {
-    return new Task(() => this.run().then(f))
-  }
-}
-
-// const myPromise = Promise.resolve('Hello Task!')
-const task = new Task(() => Promise.resolve('Hello Task!'))
-task
-  .map(console.log.bind(console))
-  .run()
