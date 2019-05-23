6c645947a3df3ea0628eaf72effe739c23ee0cea
Jose Balado <JoseBalado@users.noreply.github.com>
2019-05-23 18:29:32 +0100

    Exercise 8.1. Implement the following semigroups

--- 8_Semigroup.ts
+++ 8_Semigroup.ts
@@ -6,3 +6,51 @@ const sum: Semigroup<number> = {
   concat: (x, y) => x + y
 }
 
+console.log(
+'Sum:',
+sum.concat(4, 5)
+)
+
+const product: Semigroup<number> = {
+  concat: (x, y) => x * y
+}
+
+console.log(
+'Product:',
+product.concat(4, 5)
+)
+
+// Exercise 8.1. Implement the following semigroups
+// (string, +)
+// (boolean, ||)
+// (object, ...) (spread operator)
+
+const string: Semigroup<string> = {
+  concat: (x, y) => x + y
+}
+
+console.log(
+'Concat string:',
+string.concat('Hello ', 'world!')
+)
+
+// (boolean, &&)
+const boolean: Semigroup<boolean> = {
+  concat: (x, y) => x || y
+}
+
+console.log(
+'Concat boolean true || false:',
+boolean.concat(true, false)
+)
+
+// (object, ...) (spread operator)
+const object: Semigroup<object> = {
+  concat: (x, y) => ({ ...x, ...y })
+}
+
+console.log(
+'Concat object:',
+object.concat({ foo: 'bar', x: 42 }, { foo: 'baz', y: 13 })
+)
+
